90e4d2ad9454198b692c21b85c31ebf60d2528ae
Adrian Grimm <usmcamgrimm@gmail.com>
2020-08-07 15:37:45 -0400

    Getting set up to add data

---
+++ js/schedule.js
@@ -0,0 +1,13 @@
+fetch("https://sportspage-feeds.p.rapidapi.com/games?league=NFL&team=Indianapolis%20Colts", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "sportspage-feeds.p.rapidapi.com",
+		"x-rapidapi-key": apiKeySched
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.log(err);
+});

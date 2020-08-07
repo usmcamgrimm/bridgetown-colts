7c77fea32d8a259b7317f05d1fde3177b8482bda
Adrian Grimm <usmcamgrimm@gmail.com>
2020-08-07 18:23:24 -0400

    This isn't starting ooff so well

--- js/schedule.js
+++ js/schedule.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
+
+//let colts = document.getElementById('schedule')
+
 fetch("https://sportspage-feeds.p.rapidapi.com/games?league=NFL&team=Indianapolis%20Colts", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "sportspage-feeds.p.rapidapi.com",
-		"x-rapidapi-key": apiKeySched
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.log(err);
-});
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "sportspage-feeds.p.rapidapi.com",
+			"x-rapidapi-key": apiKeySched
+		}
+	})
+	.then(response => {
+		console.log(response);
+	})
+	.catch(err => {
+		console.log(err);
+	});
